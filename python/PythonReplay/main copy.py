06c403c26292f3a6626e5ae6f200c5e15ae5d6f8
MMotohiro <minamotowork2000@gmail.com>
2021-11-19 18:57:47 +0900

    アンケートツール改善

--- python/PythonReplay/main copy.py
+++ python/PythonReplay/main copy.py
@@ -5,6 +5,7 @@ from time import sleep
 from ltSurvey import ltSurvery
 import tkinter as tk
 from multiprocessing import Process, Pipe
+import copy
 
 sys.path .append('../')
 from py4j.java_gateway import JavaGateway, GatewayParameters, CallbackServerParameters, get_field
@@ -17,14 +18,13 @@ DOUNW_TIMER = 120
 
 def main():
     ep1,ep2 = Pipe()
-    event = Event()
-    th1 = Thread(target=replay)
-    th2 = Thread(target=emotionGUI)
+    th1 = Process(target=replay, args=(ep1,))
+    th2 = Process(target=emotionGUI, args=(ep2,))
     th1.start()
     th2.start()
     
 
-def replay():
+def replay(ep):
     gateway = JavaGateway(gateway_parameters=GatewayParameters(port=4242), callback_server_parameters=CallbackServerParameters());
     manager = gateway.entry_point
     args = sys.argv
@@ -80,14 +80,15 @@ def replay():
 
         # 自分もしくは敵のダウンを検知
         downCounter -= 1
-        print(downCounter)
         if(not framedata.getCharacter(True) is None):
             selfState = framedata.getCharacter(True).getState()
             oppState = framedata.getCharacter(False).getState()
             if((selfState.name() == "DOWN" or oppState.name() == "DOWN") and downCounter < 0):
                 print("DOWN!")
+                sleep(10)
                 downCounter = DOUNW_TIMER
-                event.set()
+                val = ep.recv()
+
 
                 
 
@@ -107,9 +108,6 @@ def replay():
 
     #dataset with dataframe
 
-
-
-
     txt_list = []
     for i in e_list:
         txt_list.append(",".join(map(str, i)))
@@ -128,13 +126,51 @@ def replay():
     gateway.close()
 
 
-def emotionGUI():
+
+#                      #
+#                      #
+#      UI   BLOCK      #
+#                      #
+#                      #
+
+
+def button_clk(ep,val):
+    def inner():
+        print(val)
+        ep.send(val)
+    return inner
+
+def emotionGUI(ep):
+    GUI_WIDTH = 960
+    GUI_HIGTH = 300
+    EMOTION_LIST = ["恐怖", "怒り", "悲しい", "嫌悪", "喜び"]
+
     root = tk.Tk()
-    root.title(u"emotion survey")
-    root.geometry("500x400")
+    root.title("emotion survey")
+    root.geometry(str(GUI_WIDTH)+"x"+str(GUI_HIGTH))
+
+    #ラベル表示
+    Label = tk.Label(text = "強\n(Strong)",width=8, height = 2, font=("", 20))
+    Label.place(x=0, y = GUI_HIGTH //6 * 2)
+    Label = tk.Label(text = "弱\n(Weak)",width=8, height = 2, font=("", 20))
+    Label.place(x=0, y = GUI_HIGTH //6 * 4)
+    Button = [[],[]]
+
+    for i,emo in enumerate(EMOTION_LIST):
+        # 強い感情
+        Button[0].append(tk.Button(text=emo,width=8, height = 2, font=("", 20)))
+        func = button_clk(ep,copy.copy(emo+"1"))
+        Button[0][-1].config(command=func)
+        Button[0][-1].place(x=GUI_WIDTH // 6 * (i+1) - 20, y = GUI_HIGTH //6 * 4)
+        #弱い感情
+        Button[1].append(tk.Button(text=emo,width=8, height = 2, font=("", 20)))
+        func = button_clk(ep,emo+"0")
+        Button[1][-1].config(command=func)
+        Button[1][-1].place(x=GUI_WIDTH // 6 * (i+1) - 20, y = GUI_HIGTH //6 * 2)
+
+
     root.mainloop()
-    event.wait()
-    event.clear()
+
 
 
 if __name__ == "__main__":
