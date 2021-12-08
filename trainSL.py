a881d05e62b77a0aa0d32b6ce36d68ab3d4b85fa
MMotohiro <minamotowork2000@gmail.com>
2021-12-09 01:55:01 +0900

    aa

--- trainSL.py
+++ trainSL.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import os, sys
 from collections import deque
 from memory import Memory
-from DQNAgent import DQNAgent
+from DQNAgent import NN
+import csv
 
 MODEL_NAME = "param.MOD05"
 MODEL_PATH = "./model/" + MODEL_NAME
@@ -17,6 +18,38 @@ def main():
     greedy_value = 1.0
 
     model = NN(action_size)
-    
-    
 
+    # read csv
+    with open() as f:
+        reader = f.read()
+        rawData = [row for row in reader]
+    #csv to numpy ndarray
+    rawData = rawData[:][:-1]
+    data = 
+    label = 
+    model.fit(data,label)
+
+
+    
+def flatten(data,emotion = None) -> np.ndarray:
+    """
+    NNに入力できるように配列に変形する
+
+    :param data: 変形したいデータ
+    :return: 変形後のデータ
+    
+    こちらも同じくCYR_AI形式に対応
+    """
+    if emotion != None:
+        result = np.zeros((1, len(data)+1 ))
+        result[0][-1] = emotion
+    else:
+        result = np.zeros((1, len(data)))
+
+    for i in range(len(data)):
+        result[0][i] = data[i]
+    
+    return result
+
+if __name__ == "__main__":
+    main()
